c6de54e96a82870f1d59418d3c056c01105ed145
JangWooJin1 <jangwoojin1@gmail.com>
2024-04-04 12:11:26 +0900

    fix: message id 오류

--- src/main/java/com/example/eldermate/entity/Message.java
+++ src/main/java/com/example/eldermate/entity/Message.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
