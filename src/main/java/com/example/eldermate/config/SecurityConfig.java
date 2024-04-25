15b39c8d4b1009e6cb3aa0e11781d853fec990d1
JangWooJin1 <jangwoojin1@gmail.com>
2024-04-25 16:57:31 +0900

    feat: swagger 코드 추가 (#23)

--- src/main/java/com/example/eldermate/config/SecurityConfig.java
+++ src/main/java/com/example/eldermate/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join").permitAll() //해당 경로들은 모든 사람들이 접근 가능
+                        .requestMatchers("/v3/**", "swagger-ui/**").permitAll()
                         .anyRequest().authenticated()); //그 외 접근은 로그인한 사용자만 접근 가능
 
 
