590bc522bde9ae97f955f7b3b0db32d210c1f95d
JangWooJin1 <jangwoojin1@gmail.com>
2024-05-31 15:12:49 +0900

    fix: 프롬프트 서비스 transactional 처리

--- src/main/java/com/example/eldermate/service/PromptService.java
+++ src/main/java/com/example/eldermate/service/PromptService.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -27,6 +28,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class PromptService {
     private final MessageRepository messageRepository;
     private final AutoTransferRepository autoTransferRepository;
